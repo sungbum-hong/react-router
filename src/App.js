bac0e6d1fc9b1655f48eb7f1291ffff1dc3943f4
Sungbeom <mkonji5466@gmail.com>
2023-04-04 15:44:36 +0900

    20230404 react-redux

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Header from './components/Header';
 import NotFound from './components/NotFound';
 import Home from './components/Home';
 import BoardDetail from './components/BoardDetail';
+import TestRedux from './components/TestRedux';
+import ListContainer from './components/ListContainer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="/board/:boardID" element={<BoardDetail />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <TestRedux />
+      <ListContainer />
+      {/* <Header /> */}
+      {/* <Routes> */}
+      {/* <Route path="/Home" element={<Home />} /> */}
+      {/* <Route path="/profile" element={<Profile />} /> */}
+      {/* <Route path="/board" element={<Board />} /> */}
+      {/* <Route path="/board/:boardID" element={<BoardDetail />} /> */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+      {/* </Routes> */}
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Header from './Header';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const weight = useSelector((state) => state.weightReducer);
   return (
     <>
-      <div>프로필 페이지 입니다!</div>
+      <div>프로필 페이지 입니다! 그런데 당신의 몸무게는 {weight}</div>
     </>
   );
 }
